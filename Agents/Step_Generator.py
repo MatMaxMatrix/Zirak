929aaceb36c98ad4755e5c534e201e91eb63e64f
Mobin Azimipanah <azimipanah.mobin@gmail.com>
2025-01-17 16:49:24 +0100

    Added agents

--- Agents/Step_Generator.py
+++ Agents/Step_Generator.py
@@ -1,7 +1,6 @@
-from autogen import ConversableAgent
 import os
-import json
-import logging
+from autogen.agentchat.assistant_agent import ConversableAgent
+
 
 
 class Similar_case_Recommendation(ConversableAgent):
@@ -59,3 +58,31 @@ Return ONLY the JSON output with no additional explanation or formatting.
                 "api_key": os.getenv("OPENAI_API_KEY"),
             },
         )
+
+
+"""
+
+from pydantic import BaseModel
+from openai import OpenAI
+
+client = OpenAI()
+
+class Step(BaseModel):
+    explanation: str
+    output: str
+
+class MathReasoning(BaseModel):
+    steps: list[Step]
+    final_answer: str
+
+completion = client.beta.chat.completions.parse(
+    model="gpt-4o-2024-08-06",
+    messages=[
+        {"role": "system", "content": "You are a helpful math tutor. Guide the user through the solution step by step."},
+        {"role": "user", "content": "how can I solve 8x + 7 = -23"}
+    ],
+    response_format=MathReasoning,
+)
+
+math_reasoning = completion.choices[0].message.parsed
+"""
